ddca2563f92c130ec3c1cb7cee1feedbb690e68e
nahum22 <nahum.nahum@gmail.com>
2024-06-13 11:13:13 +0300

    final commit

--- react/countriesOfTheWorld/src/App.jsx
+++ react/countriesOfTheWorld/src/App.jsx
@@ -1,22 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { update } from "three/examples/jsm/libs/tween.module.js";
 
 function App() {
-  const url=""
-  const [count, setCount] = useState(0);
+  const url = "https://restcountries.com/v3.1/all?fields=name";
+  // Assuming you want to store country names in state
+  const [countryNames, setCountryNames] = useState([]);
+  const [FilteredcountryNames, setFilteredCountryNames] = useState([]);
 
-const fetchData=()=>async {
+  const [input, SetInput] = useState("");
 
+  const updateFilter = (e) => {
+    SetInput(e.target.value);
 
+    const filteredNames = countryNames.filter((item) => {
+      return item.name.official.includes(input);
+    });
 
-}
+    setFilteredCountryNames(filteredNames);
+  };
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setFilteredCountryNames(data);
+      setCountryNames(data);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    }
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  return (
+    <>
+      <input type="text" onChange={updateFilter} />
 
-  return <></>;
+      <div>
+   
+        {FilteredcountryNames.map((item) => (
+          <p key={item.name.official}>{item.name.official}</p>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
