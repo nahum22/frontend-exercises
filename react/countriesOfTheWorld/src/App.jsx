a5f897d98a67a3d21244a289692a9661cb460fb1
nahum22 <nahum.nahum@gmail.com>
2024-06-13 10:07:49 +0300

    project

---
+++ react/countriesOfTheWorld/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const url=""
+  const [count, setCount] = useState(0);
+
+const fetchData=()=>async {
+
+
+
+}
+
+
+
+
+  return <></>;
+}
+
+export default App;
