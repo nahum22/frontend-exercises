f04aa719694341b8755d54aaf9009edd3cbefac7
nahum22 <nahum.nahum@gmail.com>
2024-06-13 09:38:16 +0300

    project

---
+++ react/box-animation/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="Box1"></div>
+      <div className="Box2"></div>
+      <div className="Box3"></div>
+    </>
+  );
+}
+
+export default App;
